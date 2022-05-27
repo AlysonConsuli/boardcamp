fc6aaedd3b5089ac2fb81a6fa6ccb3603440440a
Alyson Consuli <consulialyson@gmail.com>
2022-05-27 14:56:05 -0300

    feat: query string for cpf on customers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -2,6 +2,12 @@ import db from "../db.js";
 
 export const getCustomers = async (req, res) => {
     try {
+        const { cpf } = req.query
+        if (cpf) {
+            const customerCpf = await db.query(`
+            SELECT * FROM customers WHERE cpf LIKE $1`, [`${cpf}%`])
+            return res.send(customerCpf.rows)
+        }
         const customers = await db.query('SELECT * FROM customers')
         res.send(customers.rows)
     } catch {
