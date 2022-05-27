4f9356365fd1f43b7a3f46839c5393fb46fbda8a
Alyson Consuli <consulialyson@gmail.com>
2022-05-27 13:22:17 -0300

    feat: query string for games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -2,6 +2,14 @@ import db from "../db.js";
 
 export const getGames = async (req, res) => {
     try {
+        const name = req.query.name?.toLowerCase()
+        if (name) {
+            const gamesName = await db.query(`
+            SELECT games.*, categories.name as "categoryName" FROM games
+            JOIN categories ON games."categoryId" = categories.id
+            WHERE LOWER (games.name) LIKE $1`, [`${name}%`])
+            return res.send(gamesName.rows)
+        }
         const games = await db.query(`
         SELECT games.*, categories.name as "categoryName" FROM games 
         JOIN categories ON games."categoryId" = categories.id
