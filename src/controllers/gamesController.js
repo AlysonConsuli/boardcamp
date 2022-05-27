40ba9be0547d32a54e20af7f0f55c087bfc5cbc7
Alyson Consuli <consulialyson@gmail.com>
2022-05-27 12:45:16 -0300

    feat: join games w/ categories

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -2,7 +2,10 @@ import db from "../db.js";
 
 export const getGames = async (req, res) => {
     try {
-        const games = await db.query('SELECT * FROM games')
+        const games = await db.query(`
+        SELECT games.*, categories.name as "categoryName" FROM games 
+        JOIN categories ON games."categoryId" = categories.id
+        `)
         res.send(games.rows)
     } catch {
         res.sendStatus(500)
