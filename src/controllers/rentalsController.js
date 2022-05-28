b8009e89e0cdf7f8b8c25078ec450a189e790c07
Alyson Consuli <consulialyson@gmail.com>
2022-05-28 00:07:54 -0300

    feat: query string for rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,6 +1,7 @@
 import db from "../db.js";
 
 export const getRentals = async (req, res) => {
+    const { customerId, gameId } = req.query
     try {
         const rentals = await db.query(`
         SELECT rentals.*, customers.name as "customerName", 
@@ -11,13 +12,32 @@ export const getRentals = async (req, res) => {
         JOIN categories ON games."categoryId" = categories.id`)
         let rentalsResult = []
         for (let rental of rentals.rows) {
-            const { id, customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee,
+            const { id, rentDate, daysRented, returnDate, originalPrice, delayFee,
                 customerName, gameName, categoryId, categoryName } = rental
             rentalsResult.push({
-                id, customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee,
-                customer: { id: customerId, name: customerName },
-                game: { id: gameId, name: gameName, categoryId, categoryName }
+                id, customerId: rental.customerId, gameId: rental.gameId,
+                rentDate, daysRented, returnDate, originalPrice, delayFee,
+                customer: { id: rental.customerId, name: customerName },
+                game: { id: rental.gameId, name: gameName, categoryId, categoryName }
             })
+        }
+        if (customerId && gameId) {
+            const rentalsGameAndCustomer = rentalsResult.filter(rental => {
+                return (rental.customerId === Number(customerId) && rental.gameId === Number(gameId))
+            })
+            return res.send(rentalsGameAndCustomer)
+        }
+        if (customerId) {
+            const rentalsCustomer = rentalsResult.filter(rental => {
+                return (rental.customerId === Number(customerId))
+            })
+            return res.send(rentalsCustomer)
+        }
+        if (gameId) {
+            const rentalsGame = rentalsResult.filter(rental => {
+                return (rental.gameId === Number(gameId))
+            })
+            return res.send(rentalsGame)
         }
         res.send(rentalsResult)
     } catch {
